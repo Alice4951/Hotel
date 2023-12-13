184205cd673a0b583dfc7e6e3247efcf63d52436
sirineyi <144223524+sirineyi@users.noreply.github.com>
2023-12-13 11:07:21 +0100

    formulaire

--- formulaire.php
+++ formulaire.php
@@ -9,15 +9,14 @@
         <h1>Exemple de service</h1>
         <form method="post" action="insert.php">
             Chambre <input type="radio" name="type" /><br> Prix: <ins>300€ pour une chambre</ins><br>
-            Suite <input type="radio" name="type" /><br> Prix: <ins>2 personne: 400€, 3 personnes: 500€</ins><br>
-            Nombre de chambre <input type="number" name="Nombre de chambre"/><br> Nombre de suite <input type="number" name="Nombre de suite"/><br>
-            Type de chambre: Simple <input type="radio" name="chambre"/> Double <input type="radio" name="chambre"> Suite <input type="radio" name="chambre"><br>
-            Si suite: 2 personnes: <input type="radio" name="suite"/> 3 personnes: <input type="radio" name="suite"/><br>
+            Suite <input type="radio" name="type" /><br> Prix : 2 personnes : <ins> 400€ <br> 3 personnes : </ins> 500€<br>
+            Type de chambre: <input type="radio" name="chambre"/> Simple  <input type="radio" name="chambre"> Double <br>
+            Si  vous avez pris une Suite: <input type="radio" name="suite"/> : 2 personnes <input type="radio" name="suite"/> : 3 personnes <br>
             Options:<br>
             Baignoire <input type="checkbox" name="Baignoire"/> Balcon <input type="checkbox" name="Balcon"><br>
             Mini bar <input type="checkbox" name="Mini bar"/> Télévision <input type="checkbox" name="Télévision"><br>
             <label>
-            Délai du séjour du <input type="date"/> à <input type="date"/><br>
+            Délai du séjour du <input type="date"/> au <input type="date"/><br>
             </label>
         </form>
     </body>
