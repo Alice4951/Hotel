e63e928fb687b44ca7b1cc7bb44ba9df7beababe
Alice4951 <tongalice532@gmail.com>
2024-01-10 08:42:26 +0100

    meta

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 <html lang="fr-FR">
 
-<head> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Nelumbo Hotel</title>
     <link rel="stylesheet" href="style.css">
 </head>
--- mentions_legales.php
+++ mentions_legales.php
@@ -3,6 +3,9 @@
 <html lang="fr-FR">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Mentions légales de Onyx Raven Hotel</title>
 </head>
 <body>
