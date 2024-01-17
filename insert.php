876c2c92f0bb5563b0a47a9b3fa7383b4ec20b8e
Alice4951 <tongalice532@gmail.com>
2024-01-17 10:58:09 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,9 +3,9 @@
     require('credentials.php');
     $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
 
-    if (isset($_POST['nom']) || isset($_POST['texte']))
+    if (isset($_POST['chambre']) || isset($_POST['suite']))
     {
-      $chaine = "insert into idea (nom, texte) values('" . $_POST['nom'] . "', '" . $_POST['texte'] . "')";
+      $chaine = "insert into idea (chambre, suite) values('" . $_POST['chambre'] . "', '" . $_POST['suite'] . "')";
       $requete = $connexion->prepare($chaine);
       $resultat = $requete->execute();
     }
