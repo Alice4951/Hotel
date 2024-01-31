e9ffccfbf880491139cf95e41b571c3b579e3cb7
Alice4951 <tongalice532@gmail.com>
2024-01-31 14:38:03 +0100

    Insertion

--- formulaire_traitement.php
+++ formulaire_traitement.php
@@ -1,9 +1,25 @@
 <?php 
     print($_POST['Catégorie']);
     print($_POST['Pers']);
-    print($_POST['MiniBar']);
-    print($_POST['Télévision']);
-    print($_POST['Baignoire']);
-    print($_POST['Vue']);
-    print($_POST['Balcon']);
+
+    if (isset($_POST['MiniBar']))
+    {
+        print($_POST['MiniBar']);
+    }
+    if (isset($_POST['Télévision']))
+    {
+        print($_POST['Télévision']);
+    }
+    if (isset($_POST['Baignoire']))
+    {
+        print($_POST['Baignoire']);
+    }
+    if (isset($_POST['Vue']))
+    {
+        print($_POST['Vue']);
+    }
+    if (isset($_POST['Balcon']))
+    {
+        print($_POST['Balcon']);
+    }
 ?>
