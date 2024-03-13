02b274b680cd529c42c22fdc88bdad109d4f93e4
Alice4951 <tongalice532@gmail.com>
2024-03-13 10:48:27 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,10 +1,10 @@
 <?php
     require('credentials.php');
-    $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset, $user, $password");
+    $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
 
     if (isset($_POST['Catégorie']) && isset($_POST['Type']) && isset ($_POST['Options']) && isset ($_POST['Prix']))
     {
-      $chaine = "insert into article (Catégorie, Type, Options, Prix) values('" . $_POST['Catégorie'] . "','" . $_POST['Type'] . "', '" . $_POST['Options'] . "', '" . $_POST['Prix'] . "')";
+      $chaine = "insert into article (Catégorie, Type, Options, Prix) values('" . $_POST['Catégorie'] . "', '" . $_POST['Type'] . "', '" . $_POST['Options'] . "', '" . $_POST['Prix'] . "')";
       $requete = $connexion->prepare($chaine);
       $resultat = $requete->execute();
     }
